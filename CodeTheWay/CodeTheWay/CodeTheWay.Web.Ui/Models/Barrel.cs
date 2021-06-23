a5d753ba980f0d75ab78c246615dac6704bb9c1a
Conlan <cgotzion@gmail.com>
2021-06-23 12:43:52 -0500

    Running Into Issues

---
+++ CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Models/Barrel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace CodeTheWay.Web.Ui.Models
+{
+    public class Barrel
+    {
+        public Guid Id { get; set; }
+
+        public double Weight { get; set; }
+        public double Radius { get; set; }
+
+        public double Height { get; set; }
+
+
+
+        public string Contents { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+
+        public string SpecialInstruction { get; set; }
+        public bool isShipping { get; set; }
+
+
+
+
+        public double GetLongitude { get; set; }
+        public double GetLatitude { get; set; }
+
+    }
+}
