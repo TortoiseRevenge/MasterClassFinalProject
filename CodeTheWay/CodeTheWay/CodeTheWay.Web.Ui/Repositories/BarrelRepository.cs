e5acf43ea534c0052a377025d07ee6b3a5269844
Anna <22huanga@elmbrookstudents.org>
2021-06-23 13:45:37 -0500

    Updated

--- CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Repositories/BarrelRepository.cs
+++ CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Repositories/BarrelRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeTheWay.Web.Ui.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeTheWay.Web.Ui.Repositories
 {
