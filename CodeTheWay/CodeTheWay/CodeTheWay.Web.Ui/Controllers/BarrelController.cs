db7f2754f475a1965476edb75ed189501a370a75
Anna <22huanga@elmbrookstudents.org>
2021-06-24 12:28:08 -0500

    Edit

--- CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Controllers/BarrelController.cs
+++ CodeTheWay/CodeTheWay/CodeTheWay.Web.Ui/Controllers/BarrelController.cs
@@ -48,9 +48,25 @@ namespace CodeTheWay.Web.Ui.Controllers
 
         public async Task<IActionResult> Edit(Guid id)
         {
-            var Barrel = await BarrelService.GetBarrel(id);
-            return View(Barrel);
+            var result = await BarrelService.GetBarrel(id);
+            BarrelViewModel barrel = new BarrelViewModel()
+            {
+                Id = id,
+                Weight = result.Weight,
+                Radius = result.Radius,
+                Height = result.Height,
+                Contents = result.Contents,
+                Owner = result.Owner,
+                Sender = result.Sender,
+                Receiver = result.Receiver,
+                GetLongitude = result.GetLongitude,
+                GetLatitude = result.GetLatitude,
+                SpecialInstruction = result.SpecialInstruction,
+                isShipping = result.isShipping
+            };
+            return View(barrel);
         }
+
         public async Task<IActionResult> Update(Barrel model) {
             if (ModelState.IsValid) {
                 var Barrel = await BarrelService.Update(model);
